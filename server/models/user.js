3fa151898f1155f7b0293161593d66e5c7d5581b
Umar <umar.hussain2924@gmail.com>
2024-07-27 21:45:31 +0500

    added user model

---
+++ server/models/user.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const Schema= mongoose.Schema
+
+const User = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    zip :{
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    }
+
+});
+
+User.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+User.set('toJSON', {
+    virtuals: true,
+});
+
+export default mongoose.model("User", User)
